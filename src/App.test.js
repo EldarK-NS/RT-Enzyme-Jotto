4ed89f2bcd530a51ad79a2b5c2e87bdb0ddb14b7
EldarK-NS <eldark.ns@gmail.com>
2021-10-19 20:00:00 +0600

    Set up Redux

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,35 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
+import { getSecretWord as mockGetSecretWord } from "./actions";
+
+//activate global mock to make sure getSecretWord doesn't make network call
+jest.mock("./actions");
 
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test("renders without error", () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
+});
+
+describe("get secret word", () => {
+  beforeEach(() => {
+    //clear the mock from previous test
+    mockGetSecretWord.mockClear();
+  });
+  test("getSecretWord on app mount", () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test("getSecretWord does not run on app update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    //using setProps because wrapper.update()  doesn't trigger useEffect
+    wrapper.setProps();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} state - array of guessed words
+ * @param {array} action - action to be reduced
+ * @retutn {bollean} new success state
+ */
+
+export default (state, action) => {
+  return null;
+};
